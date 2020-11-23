a206fcb03ff36d9d2d9f196166ff3d3111448531
Sarah Koop <skoop@paypal.com>
2020-11-23 14:23:46 -0600

    Update visa checkout activity listener to expect a payment method nonce on success.
    Signed-off-by: Steven Shropshire <steven.shropshire@getbraintree.com>

--- VisaCheckout/src/main/java/com/braintreepayments/api/VisaCheckoutActivityResultListener.java
+++ VisaCheckout/src/main/java/com/braintreepayments/api/VisaCheckoutActivityResultListener.java
@@ -1,4 +1,7 @@
 package com.braintreepayments.api;
 
+import com.braintreepayments.api.models.PaymentMethodNonce;
+
 public interface VisaCheckoutActivityResultListener {
+    void onResult(Exception error, PaymentMethodNonce paymentMethodNonce);
 }
