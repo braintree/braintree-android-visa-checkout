883540660c0d16a7537e464d8da848c4c446ee27
Steven Shropshire <steven.shropshire@getbraintree.com>
2020-12-03 11:03:06 -0600

    Refactor to use wrapped Visa Checkout Button
    Signed-off-by: Sarah Koop <skoop@paypal.com>

--- VisaCheckout/src/main/java/com/braintreepayments/api/VisaCheckoutButton.java
+++ VisaCheckout/src/main/java/com/braintreepayments/api/VisaCheckoutButton.java
@@ -3,26 +3,41 @@ package com.braintreepayments.api;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.braintreepayments.api.models.PaymentMethodNonce;
 import com.braintreepayments.api.visacheckout.R;
+import com.visa.checkout.CheckoutButton;
+import com.visa.checkout.Profile;
+import com.visa.checkout.PurchaseInfo;
+import com.visa.checkout.VisaCheckoutSdk;
+import com.visa.checkout.VisaPaymentSummary;
 
 public class VisaCheckoutButton extends FrameLayout {
 
     public VisaCheckoutButton(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        initView();
+        prepareView();
     }
 
     public VisaCheckoutButton(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initView();
+        prepareView();
     }
 
     public VisaCheckoutButton(Context context) {
         super(context);
-        initView();
+        prepareView();
     }
 
-    private void initView() {
+    private void prepareView() {
         inflate(getContext(), R.layout.visa_checkout_button_wrapper, this);
+    }
+
+    public void init(FragmentActivity activity, Profile.ProfileBuilder profileBuilder, PurchaseInfo.PurchaseInfoBuilder purchaseInfo, VisaCheckoutSdk.VisaCheckoutResultListener visaCheckoutResultListener) {
+        CheckoutButton checkoutButton = findViewById(R.id.internal_visa_checkout_button);
+
+        checkoutButton.init(activity, profileBuilder.build(), purchaseInfo.build(), visaCheckoutResultListener);
     }
 }
